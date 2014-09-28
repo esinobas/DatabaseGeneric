bef307363d017227e9b3a76e228ce44dc99654b2
Esteban Sinobas Carpio <tebi@Ubuntu-Webserver.(none)>
2014-09-28 22:17:19 +0200

    Changed directories path

--- Tools/GenerateDatabase/GenerateTable.php
+++ Tools/GenerateDatabase/GenerateTable.php
@@ -3,66 +3,13 @@
     * Script makes a template php file witha a object or class that allows the
     * database access
     */
-   set_include_path( get_include_path() . PATH_SEPARATOR . "../");
+   set_include_path( get_include_path() . PATH_SEPARATOR . 
+   dirname(__FILE__)."/../../..");
    
-   include_once '../LoggerMgr/LoggerMgr.php';
+   include_once 'LoggerMgr/LoggerMgr.php';
    $logger = LoggerMgr::Instance()->getLogger("main");
   
-   function writeHeader($theFileHandler){
-      global $logger;
-      
-      $logger->trace("Enter");
-      
-      $text = "<?php\n";
-      $text.= "   /**\n";
-      $text.= "    * Class where the logical and physical database is defined.\n";
-      $text.= "    * Also It is defined the mapping between both schemas.\n";
-      $text.= "    * In this class are the tables definition in logical and physical level\n".
-      $text.= "    *";
-      $text.= "    * The class has 2 parts:\n";
-      $text.= "    *    1.- The table and columns names.\n";
-      $text.= "    *       The names are constans\n";
-      $text.= "    *    2.- The tables definitions using the declared names previously\n";
-      $text.= "    */\n";
-      $text.= "\n";
-      $text.= "   include_once dirname(__FILE__).'/Core/TableDef.php';\n";
-      $text.= "   include_once dirname(__FILE__).'/Core/ColumnDef.php';\n";
-      $text.= "   include_once dirname(__FILE__).'/Core/ColumnType.php';\n";
-      $text.= "\n";
-      $text.= "   include_once 'LoggerMgr/LoggerMgr.php';\n";
-      $text.= "\n";
-      fwrite($theFileHandler, $text);
-     
-      $logger->trace("Exit");
-   }
-   
-   function writeClassDefinition($theFileHandler, $theClassName, $theColumns){
-      global $logger;
-      $logger->trace("Enter");
-      $text = "   class $theClassName {\n";
-      $text .= "\n";
-      $text .= "     private \$loggerM;\n";
-      $text .= "\n";
-      
-      $logger->debug("Write the constans name and name columns");
-      
-      $text .= "     /*\n";
-      $text .= "      * Contant Table Name\n";
-      $text .= "      */\n";
-      $text .= "     const ".$theClassName."TableC = \"$theClassName\";\n";
-      $text .= "\n";
-      $text .= "     /*\n";
-      $text .= "      * Contants table columns\n";
-      $text .= "      */\n";
-      for ($idx= 0; $idx <count($theColumns); $idx++){
-         $text .= "     const ".$theColumns[$idx]->name."ColumnC = ".
-                                   "\"".$theColumns[$idx]->name."\";\n";
-      }
-      
-      
-      fwrite($theFileHandler, $text);
-      $logger->trace("Exit");
-   }
+  
    
    function closeClassDefinition($theFileHandler){
       global $logger;
@@ -177,18 +124,14 @@
    $logger->info("Starting to read the file $file");
    $definitions = simplexml_load_file($file);
    
+   $logger->info("Read the database tables definition");
    $logger->debug("The file contains ".count($definitions->table_definition)." table definitions.");
    for ($idx = 0; $idx < count($definitions->table_definition); $idx++){
       $fileName = $outDir.$definitions->table_definition[$idx]->name.".php";
       $logger->trace("Writing file: $fileName");
       
       $fileHandler = fopen($fileName, "w");
-      /*writeHeader($fileHandler);
-      writeClassDefinition($fileHandler, 
-                       $definitions->table_definition[$idx]->name,
-                       $definitions->table_definition[$idx]->column);
-      closeClassDefinition($fileHandler);
-      */
+      
       writeHeaderTemplate($fileHandler, 
                        $definitions->table_definition[$idx]->name);
       writeColumnsConstanDefinition($fileHandler,
